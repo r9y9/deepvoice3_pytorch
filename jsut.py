130543035fd0b501b127d9f8b679d487c88b379f
Ryuichi Yamamoto <zryuichi@gmail.com>
2017-11-08 12:48:57 +0900

    Preprocessor for ljspeech and jsut

---
+++ jsut.py
@@ -0,0 +1,42 @@
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
+import numpy as np
+import os
+import audio
+from nnmnkwii.datasets import jsut
+
+
+def build_from_path(in_dir, out_dir, num_workers=1, tqdm=lambda x: x):
+    executor = ProcessPoolExecutor(max_workers=num_workers)
+    futures = []
+
+    transcriptions = jsut.TranscriptionDataSource(
+        in_dir, subsets=jsut.available_subsets).collect_files()
+    wav_paths = jsut.WavFileDataSource(
+        in_dir, subsets=jsut.available_subsets).collect_files()
+
+    for index, (text, wav_path) in enumerate(zip(transcriptions, wav_paths)):
+        futures.append(executor.submit(
+            partial(_process_utterance, out_dir, index + 1, wav_path, text)))
+    return [future.result() for future in tqdm(futures)]
+
+
+def _process_utterance(out_dir, index, wav_path, text):
+    # Load the audio to a numpy array:
+    wav = audio.load_wav(wav_path)
+
+    # Compute the linear-scale spectrogram from the wav:
+    spectrogram = audio.spectrogram(wav).astype(np.float32)
+    n_frames = spectrogram.shape[1]
+
+    # Compute a mel-scale spectrogram from the wav:
+    mel_spectrogram = audio.melspectrogram(wav).astype(np.float32)
+
+    # Write the spectrograms to disk:
+    spectrogram_filename = 'jsut-spec-%05d.npy' % index
+    mel_filename = 'jsut-mel-%05d.npy' % index
+    np.save(os.path.join(out_dir, spectrogram_filename), spectrogram.T, allow_pickle=False)
+    np.save(os.path.join(out_dir, mel_filename), mel_spectrogram.T, allow_pickle=False)
+
+    # Return a tuple describing this training example:
+    return (spectrogram_filename, mel_filename, n_frames, text)
---
+++ preprocess.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+"""
+Preprocess dataset
+
+usage: preprocess.py [options] <name> <in_dir> <out_dir>
+
+options:
+    --num_workers=<n>        Num workers.
+    -h, --help               Show help message.
+"""
+from docopt import docopt
+import os
+from multiprocessing import cpu_count
+from tqdm import tqdm
+from hparams import hparams
+
+
+def preprocess_ljspeech(in_dir, out_root, num_workers):
+    import ljspeech
+    os.makedirs(out_dir, exist_ok=True)
+    metadata = ljspeech.build_from_path(in_dir, out_dir, num_workers, tqdm=tqdm)
+    write_metadata(metadata, out_dir)
+
+
+def preprocess_jsut(in_dir, out_root, num_workers):
+    import jsut
+    os.makedirs(out_dir, exist_ok=True)
+    metadata = jsut.build_from_path(in_dir, out_dir, num_workers, tqdm=tqdm)
+    write_metadata(metadata, out_dir)
+
+
+def write_metadata(metadata, out_dir):
+    with open(os.path.join(out_dir, 'train.txt'), 'w', encoding='utf-8') as f:
+        for m in metadata:
+            f.write('|'.join([str(x) for x in m]) + '\n')
+    frames = sum([m[2] for m in metadata])
+    hours = frames * hparams.frame_shift_ms / (3600 * 1000)
+    print('Wrote %d utterances, %d frames (%.2f hours)' % (len(metadata), frames, hours))
+    print('Max input length:  %d' % max(len(m[3]) for m in metadata))
+    print('Max output length: %d' % max(m[2] for m in metadata))
+
+
+if __name__ == "__main__":
+    args = docopt(__doc__)
+    name = args["<name>"]
+    in_dir = args["<in_dir>"]
+    out_dir = args["<out_dir>"]
+    num_workers = args["--num_workers"]
+    num_workers = cpu_count() if num_workers is None else num_workers
+
+    if name == 'jsut':
+        preprocess_jsut(in_dir, out_dir, num_workers)
+    elif name == 'ljspeech':
+        preprocess_ljspeech(in_dir, out_dir, num_workers)
+    else:
+        assert False
