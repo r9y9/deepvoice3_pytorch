6ff6aacb86ca9d475073014c9e96da5a36f8ddf6
Ryuichi Yamamoto <zryuichi@gmail.com>
2017-11-18 22:15:27 +0900

    lr schedule

--- lrschedule.py
+++ lrschedule.py
@@ -2,9 +2,9 @@ import numpy as np
 
 
 # https://github.com/tensorflow/tensor2tensor/issues/280#issuecomment-339110329
-def noam_learning_rate_decay(init_lr, global_step):
+def noam_learning_rate_decay(init_lr, global_step, warmup_steps=4000):
      # Noam scheme from tensor2tensor:
-    warmup_steps = 4000.0
+    warmup_steps = float(warmup_steps)
     step = global_step + 1.
     lr = init_lr * warmup_steps**0.5 * np.minimum(
         step * warmup_steps**-1.5, step**-0.5)
