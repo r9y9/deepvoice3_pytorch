f8975bb0c3954af164f04f6a613646e660e488b1
Ryuichi Yamamoto <zryuichi@gmail.com>
2017-11-22 01:44:37 +0900

    Add cyclic annealing

--- lrschedule.py
+++ lrschedule.py
@@ -15,3 +15,21 @@ def step_learning_rate_decay(init_lr, global_step,
                              anneal_rate=0.98,
                              anneal_interval=30000):
     return init_lr * anneal_rate ** (global_step // anneal_interval)
+
+
+def cyclic_cosine_annealing(init_lr, global_step, T, M):
+    """Cyclic cosine annealing
+
+    https://arxiv.org/pdf/1704.00109.pdf
+
+    Args:
+        init_lr (float): Initial learning rate
+        global_step (int): Current iteration number
+        T (int): Total iteration number (i,e. nepoch)
+        M (int): Number of ensembles we want
+
+    Returns:
+        float: Annealed learning rate
+    """
+    TdivM = T // M
+    return init_lr / 2.0 * (np.cos(np.pi * ((global_step - 1) % TdivM) / TdivM) + 1.0)
