52d1026d477c39f2dd12761c461863e732ce86fc
amilamad <amilamad@gmail.com>
2018-03-10 12:07:47 +0530

    Minor fix on Windows (#56)
    * Update synthesis.py
    
    fix for error ,
    TypeError: torch.index_select received an invalid combination of arguments - got (torch.cuda.FloatTensor, int, torch.cuda.IntTensor), but expected (torch.cuda.FloatTensor source, int dim, torch.cuda.LongTensor index)
    on windows.
    mentioned in the issue #37
    
    * Added missing import hparams_debug_string

--- preprocess.py
+++ preprocess.py
@@ -15,7 +15,7 @@ import os
 from multiprocessing import cpu_count
 from tqdm import tqdm
 import importlib
-from hparams import hparams
+from hparams import hparams, hparams_debug_string
 
 
 def preprocess(mod, in_dir, out_root, num_workers):
